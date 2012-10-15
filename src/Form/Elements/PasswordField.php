8ff71b4241ba7896415897d868a654ec2c3478c9
Koosha Khajeh Moogahi <koosha.khajeh@gmail.com>
2012-10-15 13:17:48 +0330

    Add phpdoc to PasswordField.php

--- src/Form/Elements/PasswordField.php
+++ src/Form/Elements/PasswordField.php
@@ -1,9 +1,44 @@
 <?php
+/**
+  * Class implementing PasswordField element
+  * 
+  * PHP version 5.3
+  *
+  * @category  Xamin
+  * @package   XaminApplianceMarket
+  * @author    Alireza Ghafouri <fzerorubigd@gmail.com>
+  * @author    Behrooz Shabani <everplays@gmail.com>
+  * @author    Koosha Khajeh Moogahi <koosha.khajeh@gmail.com>
+  * @copyright 2012 (c) ParsPooyesh Co
+  * @license   GNU GPLv3+ <http://www.gnu.org/licenses/gpl-3.0.html>
+  * @link      http://www.xamin.ir
+  *
+  */
+
+/**
+  * Class implementing PasswordField element
+  *
+  * @category  Xamin
+  * @package   XaminApplianceMarket
+  * @author    Alireza Ghafouri <fzerorubigd@gmail.com>
+  * @author    Behrooz Shabani <everplays@gmail.com>
+  * @author    Koosha Khajeh Moogahi <koosha.khajeh@gmail.com>
+  * @copyright 2012 (c) ParsPooyesh Co
+  * @license   GNU GPLv3+ <http://www.gnu.org/licenses/gpl-3.0.html>
+  * @link      http://www.xamin.ir
+  *
+  */
+
 
 class Form_Elements_PasswordField extends Form_Elements_TextField
 {
     /**
      * constructs element
+     *
+     * @param array                  $configuration optional configuration array
+     * @param Form_Elements_Fieldset $form          From (default to NULL)
+     *
+     * @return an object
      */
     public function __construct($configuration=null, Form_Elements_Fieldset $form=null)
     {
