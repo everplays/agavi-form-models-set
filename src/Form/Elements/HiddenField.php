fec85012a33306e40bc79645702607d04b57f669
Koosha Khajeh Moogahi <koosha.khajeh@gmail.com>
2012-10-15 13:16:53 +0330

    Add phpdoc to HiddenField.php

--- src/Form/Elements/HiddenField.php
+++ src/Form/Elements/HiddenField.php
@@ -1,4 +1,33 @@
 <?php
+/**
+  * Class implementing HiddenField element
+  * 
+  * PHP version 5.3
+  *
+  * @category  Xamin
+  * @package   XaminApplianceMarket
+  * @author    Alireza Ghafouri <fzerorubigd@gmail.com>
+  * @author    Behrooz Shabani <everplays@gmail.com>
+  * @author    Koosha Khajeh Moogahi <koosha.khajeh@gmail.com>
+  * @copyright 2012 (c) ParsPooyesh Co
+  * @license   GNU GPLv3+ <http://www.gnu.org/licenses/gpl-3.0.html>
+  * @link      http://www.xamin.ir
+  *
+  */
+
+/**
+  * Class implementing HiddenField element
+  *
+  * @category  Xamin
+  * @package   XaminApplianceMarket
+  * @author    Alireza Ghafouri <fzerorubigd@gmail.com>
+  * @author    Behrooz Shabani <everplays@gmail.com>
+  * @author    Koosha Khajeh Moogahi <koosha.khajeh@gmail.com>
+  * @copyright 2012 (c) ParsPooyesh Co
+  * @license   GNU GPLv3+ <http://www.gnu.org/licenses/gpl-3.0.html>
+  * @link      http://www.xamin.ir
+  *
+  */
 
 class Form_Elements_HiddenField extends Form_Elements_TextField
 {
