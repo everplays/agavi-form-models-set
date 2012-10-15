b697bb450b70433bd1d582b1ca986883ee936eab
Koosha Khajeh Moogahi <koosha.khajeh@gmail.com>
2012-10-15 13:20:11 +0330

    Add phpdoc to ValidationException.php

--- src/Form/ValidationException.php
+++ src/Form/ValidationException.php
@@ -1,5 +1,34 @@
 <?php
 
+/**
+  * Validation Exception class
+  * 
+  * PHP version 5.3
+  *
+  * @category  Xamin
+  * @package   XaminApplianceMarket
+  * @author    Alireza Ghafouri <fzerorubigd@gmail.com>
+  * @author    Behrooz Shabani <everplays@gmail.com>
+  * @author    Koosha Khajeh Moogahi <koosha.khajeh@gmail.com>
+  * @copyright 2012 (c) ParsPooyesh Co
+  * @license   GNU GPLv3+ <http://www.gnu.org/licenses/gpl-3.0.html>
+  * @link      http://www.xamin.ir
+  *
+  */
+
+/**
+  * Validation Exception class
+  *
+  * @category  Xamin
+  * @package   XaminApplianceMarket
+  * @author    Alireza Ghafouri <fzerorubigd@gmail.com>
+  * @author    Behrooz Shabani <everplays@gmail.com>
+  * @author    Koosha Khajeh Moogahi <koosha.khajeh@gmail.com>
+  * @copyright 2012 (c) ParsPooyesh Co
+  * @license   GNU GPLv3+ <http://www.gnu.org/licenses/gpl-3.0.html>
+  * @link      http://www.xamin.ir
+  *
+  */
 class Form_ValidationException extends Exception
 {
     /**
@@ -9,6 +38,8 @@ class Form_ValidationException extends Exception
 
     /**
      * constcutor
+     *
+     * @param array $errors validation errors
      */
     public function __construct(array $errors)
     {
